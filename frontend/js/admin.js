728ded028ea0e43b22b4ffe7e9063d079cf8eb31
Wilson Ramos <hupcontato@gmail.com>
2020-04-27 11:46:33 -0300

    Funcionalidades na pagina Admin

---
+++ frontend/js/admin.js
@@ -0,0 +1,29 @@
+const select = document.querySelector('[data-select]');
+
+listarPosts()
+.then( post => {
+   
+   post.forEach( post => {
+
+      const option = document.createElement('option');
+      option.textContent = post.titulo;
+      option.value = post._id;
+
+      select.appendChild(option);
+
+   })
+})
+
+const carregarPost = () => {
+   const titulo = document.querySelector('.titulo-admin');
+   const paragrafo = document.querySelector('.paragrafo-admin');
+
+
+   listarPost(select.value)
+   .then( post => {
+
+      titulo.value = post.titulo;
+      paragrafo.value = post.paragrafo;
+   })
+}
+
